01b06afb99e793b7d55088981352da0fde0c5607
kmadhu4500 <kmadhurendra59@gmail.com>
2025-06-22 20:42:24 +0530

    Update: Refactored components and improved UI logic

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -23,15 +23,11 @@ console.log('Song object:', song);
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
-          className={`absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
-            activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden group-hover:flex'
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+            activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'
           }`}
         >
-          <PlayPause
-          song={song}
-          handlePause={handlePauseClick}
-          handlePlay={handlePlayClick}
-          />
+          <PlayPause/>
         </div>
         <img
   alt="song_img"
